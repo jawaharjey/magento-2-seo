c14adaa9e10e59fedf143d11dcdd1172f432f569
Jawahar Jeyaraman <jawahar.jeyaraman@sellerdeck.com>
2020-04-13 18:40:54 +0530

    Adding comments

--- Helper/Data.php
+++ Helper/Data.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright © 2020 Betzal. All Rights Reserved..
+ * Author: Jawahar Jeyaraman (jeyaraman.jawahar@gmail.com)
  */
 namespace Betzal\Seo\Helper;
 
